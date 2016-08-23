3266ee95251cbae64f4d93aa11403122cd2be23b
meeniebeanie <minyen.j@gmail.com>
2016-08-24 01:16:05 +0800

    seeded ingredients array data

---
+++ public/ingredients.js
@@ -0,0 +1,53 @@
+var ingredients_arr = [
+  {
+    label: "Base",
+    id: "base",
+    type: "radio",
+    options: [
+      {
+        value: 0,
+        ingredient: "White Rice",
+        calories: 123
+      },
+      {
+        value: 1,
+        ingredient: "Whole Grain Rice",
+        calories: 23
+      },
+      {
+        value: 2,
+        ingredient: "Buckwheat Noodles",
+        calories: 345
+      }
+    ] //end of base's options
+  }, //end of base
+
+  {
+    label: "Sides",
+    id: "sides",
+    type: "radio",
+    options: [
+      {
+        value: 0,
+        ingredient: "Side 1",
+        calories: 123
+      },
+      {
+        value: 1,
+        ingredient: "Side 2",
+        calories: 43
+      },
+      {
+        value: 2,
+        ingredient: "Side 3",
+        calories: 234
+      },
+      {
+        value: 3,
+        ingredient: "Side 4",
+        calories: 243
+      }
+    ]//end of sides's options
+  }//end of sides
+  
+];//end of ingredients array
